ffaaa939e4cdb719295c34a08f15d371849a9784
barbararibeiroo <barbararibeiro18@gmail.com>
2023-04-04 01:48:34 -0300

    Atualização das classes controllers, model e arquivo properties

--- rifas-backend/src/main/java/com/projetosite/rifas/model/CadastroRifa.java
+++ rifas-backend/src/main/java/com/projetosite/rifas/model/CadastroRifa.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -11,11 +12,15 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.io.Serializable;
+
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Getter
 @Setter
 @Entity
-public class CadastroRifa {
+public class CadastroRifa implements Serializable {
+
+    private static final long serialVersionUID = 1L;
 
     @EqualsAndHashCode.Include
     @Id
